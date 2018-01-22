e524b50db96e0ceb2691d69bfb63259d7ebc00aa
xuxiao01 <o-xuxiao01@beyondsoft.com>
2018-01-22 10:24:07 +0800

    ps1

---
+++ presidents/appcenter-post-clone.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/xuxiao01/A_Windows_UWP-Presidents
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
